419db893c5dcf363eb4e752a72eca76b3e334139
Jason Bukowski <jasonbukowski@users.noreply.github.com>
2017-06-28 19:04:08 -0400

    Hash argument to be always read as string for submit

--- chainpoint.js
+++ chainpoint.js
@@ -26,6 +26,7 @@ let argv = yargs
   .command('submit', 'submit a hash to be anchored', (yargs) => {
     let argv = yargs
       .usage('Usage: submit [options] (<hash> <hash>... | <hash>,<hash>,... )')
+      .string('_')
       .argv
     submitCmd.execute(yargs, argv)
   })
